734a8b3c1cb396b5f90943f410855392e0f2b6e2
disufi <d.isufi@gmail.com>
2014-10-28 23:14:34 +0100

    [new] expanded the amount of views

---
+++ Quizio/Quizio/Pages/News/Today.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quizio.Pages.News
+{
+    /// <summary>
+    /// Interaction logic for Today.xaml
+    /// </summary>
+    public partial class Today : UserControl
+    {
+        public Today()
+        {
+            InitializeComponent();
+        }
+    }
+}
