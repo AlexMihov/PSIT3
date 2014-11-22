2f080c177c5dc129a51aeefc183ad120f06c211a
disufi <d.isufi@gmail.com>
2014-11-22 23:52:47 +0100

    [refactor] async data fetching
    - worker for fetching
    - progressbar
    - better user expirience

--- Quizio/Quizio/App.xaml.cs
+++ Quizio/Quizio/App.xaml.cs
@@ -26,38 +26,7 @@ namespace Quizio
 
             if (login.ShowDialog() == true && login.granted)
             {
-                //data retrieve simulation (to be fetched from DB)
-                string name = login.userName.Text;
-                string currentStatus = "I love Quizio";
-                string location = "Jerusalem";
-
-                Friend hans = new Friend("Hans", "Muster bedumtsch");
-                Friend fritz = new Friend("Fritz", "Ritz bedumtsch");
-                Friend rudolf = new Friend("Rudolf", "Liebt Golf bedumtsch");
-                Friend michel = new Friend("Michel", "Mit der Sichel bedumtsch");
-
-                List<Friend> friends = new List<Friend>();
-                friends.Add(hans);
-                friends.Add(fritz);
-                friends.Add(rudolf);
-                friends.Add(michel);
-
-                User user = new User(name, currentStatus, friends);
-                user.Location = location;
-                user.Id = 2;
-
-                NotificationDAO natDAO = new NotificationDAO();
-                List<Notification> notifications = natDAO.loadNotifications(user.Id);
-
-                RankingDAO rankingDAO = new RankingDAO();
-                List<Ranking> rankings = rankingDAO.loadRankings();
-
-                CategoryDAO catDao = new CategoryDAO();
-                List<Category> categories = catDao.loadCategories();
-
-                MainViewModel mvm = new MainViewModel(user, categories, notifications, rankings);
-                //data retrieve simulation end
-
+                MainViewModel mvm = new MainViewModel(login.User, login.Categories, login.Notifications, login.Rankings);
                 var mainWindow = new MainWindow(mvm);
                 //Re-enable normal shutdown mode.
                 Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
--- Quizio/Quizio/Views/Dialogs/Login.xaml.cs
+++ Quizio/Quizio/Views/Dialogs/Login.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using FirstFloor.ModernUI.Windows.Controls;
+using Quizio.Models;
+using Quizio.Utilities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,12 +25,27 @@ namespace Quizio.Views.Dialogs
     /// </summary>
     public partial class Login : ModernDialog
     {
+        private BackgroundWorker bw;
+
         public string UserName { get; private set; }
         public string Password { get; private set; }
         public bool granted { get; private set; }
 
+        //datafields for mainviewmodel constructor
+        public List<Friend> Friends { get; private set; }
+        public User User { get; set; }
+        public List<Notification> Notifications { get; private set; }
+        public List<Ranking> Rankings { get; private set; }
+        public List<Category> Categories { get; private set; }
+        //end datafields for mainviewmodel constructor
+
         public Login()
         {
+            bw = new BackgroundWorker();
+            bw.WorkerSupportsCancellation = true;
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+
             InitializeComponent();
         }
 
@@ -40,17 +59,80 @@ namespace Quizio.Views.Dialogs
             UserName = userName.Text;
             Password = password.Password;
 
-            if (UserName != "" && Password != "") // To Be implemented: check with DB
+            loading.Visibility = System.Windows.Visibility.Visible;
+
+            if (bw.IsBusy != true)
+            {
+                bw.RunWorkerAsync();
+            }
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            if ((worker.CancellationPending == true))
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                if (UserName != "" && Password != "") // To Be implemented: check with DB
+                {
+                    string currentStatus = "I love Quizio";
+                    string location = "Jerusalem";
+
+                    Friend hans = new Friend("Hans", "Muster bedumtsch");
+                    Friend fritz = new Friend("Fritz", "Ritz bedumtsch");
+                    Friend rudolf = new Friend("Rudolf", "Liebt Golf bedumtsch");
+                    Friend michel = new Friend("Michel", "Mit der Sichel bedumtsch");
+
+                    Friends = new List<Friend>();
+                    Friends.Add(hans);
+                    Friends.Add(fritz);
+                    Friends.Add(rudolf);
+                    Friends.Add(michel);
+
+                    User = new User(UserName, currentStatus, Friends);
+                    User.Location = location;
+                    User.Id = 2;
+
+                    NotificationDAO natDAO = new NotificationDAO();
+                    Notifications = natDAO.loadNotifications(User.Id);
+
+                    RankingDAO rankingDAO = new RankingDAO();
+                    Rankings = rankingDAO.loadRankings();
+
+                    CategoryDAO catDao = new CategoryDAO();
+                    Categories = catDao.loadCategories();
+                }
+                else
+                {
+                    e.Cancel = true; // cancel the worker if user/pw not correct
+                }
+            }
+        }
+
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if ((e.Cancelled == true))
+            {
+                this.loading.Visibility = System.Windows.Visibility.Hidden;
+                ModernDialog.ShowMessage("Invalid Username/Password combination!", "Error", MessageBoxButton.OK);
+            }
+
+            else if (!(e.Error == null))
+            {
+                this.loading.Visibility = System.Windows.Visibility.Hidden;
+                ModernDialog.ShowMessage("Error: " + e.Error.Message, "Error", MessageBoxButton.OK);
+            }
+
+            else
             {
                 granted = true;
                 this.DialogResult = true;
                 this.Close();
             }
-            else
-            {
-                ModernDialog.ShowMessage("Invalid Username/Password combination!", "Error", MessageBoxButton.OK);
-            }
-            
         }
     }
 }
