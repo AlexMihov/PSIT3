47eef0d621ad2e26908dd2c46d964b140b84048c
neary <micha.schwendener@gmail.com>
2014-11-30 21:36:04 +0100

    [update] cleaned up and documented the friend model

--- Quizio/Quizio/Models/Friend.cs
+++ Quizio/Quizio/Models/Friend.cs
@@ -1,21 +1,39 @@
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Quizio.Models
 {
+    /// <summary>
+    /// The class friend is a model for the friends of a player of the quiz. It saves the id, the name, the status, and the location of the friend.
+    /// </summary>
     public class Friend
     {
+        /// <summary>
+        /// Gets or sets the id of a friend as an integer.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of a friend as a string.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the status of a friend as a string.
+        /// </summary>
         public string Status { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the location of a friend as a string.
+        /// </summary>
         public string Location { get; set; }
 
+        /// <summary>
+        /// Constructor of the friend class, it takes an id, a name, a status and location. It can be used to create a friend object from a json string.
+        /// </summary>
+        /// <param name="id">The id an answer has in the db as an integer</param>
+        /// <param name="name">The name of the friend as a string</param>
+        /// <param name="status">The status of the friend as a string</param>
+        /// <param name="location">The location of the friend as a string</param>
         [JsonConstructor]
         public Friend(int id, string name, string status, string location)
         {
@@ -24,10 +42,5 @@ namespace Quizio.Models
             this.Status = status;
             this.Location = location;
         }
-
-
-        public Friend(string name, string status): this(0, name, status, "") { }
-
-
     }
 }
