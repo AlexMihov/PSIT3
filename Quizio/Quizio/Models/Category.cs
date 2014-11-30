e3b205c112f381c1b5bf26f4a78fcc2a26749796
neary <micha.schwendener@gmail.com>
2014-11-30 21:28:54 +0100

    [update] cleaned up and documented the category model

--- Quizio/Quizio/Models/Category.cs
+++ Quizio/Quizio/Models/Category.cs
@@ -1,19 +1,40 @@
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Quizio.Models
 {
+    /// <summary>
+    /// The class category is a model for quiz categories. It saves a name, an id, a description and a list of quizies.
+    /// </summary>
     public class Category
     {
+        /// <summary>
+        /// Gets the name of a category as an string.
+        /// </summary>
         public string Name { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the id of category to interact with the db as an integer.
+        /// </summary>
         public int Id { get; set; }
+        
+        /// <summary>
+        /// Gets or set the description of the category as an string.
+        /// </summary>
         public string Description { get; set; } 
-        public List<Quiz> Quizies { get; set; }
+        
+        /// <summary>
+        /// Gets or sets a list of <c>Quiz</c> holding all the quizies, which belong to this category.
+        /// </summary>
+        public List<Quiz> Quizies { get; private set; }
 
+        /// <summary>
+        /// Constructor of the category class, it takes an id, a name, a description and a list of quizies. It can be used to create a Category from a json string.
+        /// </summary>
+        /// <param name="id">The id an answer has in the db as an integer</param>
+        /// <param name="name">The name of a category as a string</param>
+        /// <param name="description">The description of a category as a string</param>
+        /// <param name="quizies">A list of quizies as <c>List<c> containing <c>Quiz</c>, which belong to this category.</Quiz></c></Quiz></c></param>
         [JsonConstructor]
         public Category(int id, string name, string description, List<Quiz> quizies) 
         {
@@ -23,9 +44,12 @@ namespace Quizio.Models
             this.Quizies = quizies;
         }
 
-
+        /// <summary>
+        /// A short constructor of the category class, it takes a name, a description and a list of quizies. The id is automaticly set to zero.
+        /// </summary>
+        /// <param name="name">The name of a category as a string</param>
+        /// <param name="description">The description of a category as a string</param>
+        /// <param name="quizies">A list of quizies as <c>List<c> containing <c>Quiz</c>, which belong to this category.</param>
         public Category(string name, string description, List<Quiz> quizies) : this(0, name, description, quizies) { }
-
-        public Category(string name, List<Quiz> quizies) : this(name, "", quizies) { }
     }
 }
