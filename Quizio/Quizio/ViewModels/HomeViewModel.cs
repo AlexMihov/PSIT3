bebd72cde254edbdc66570cd0c0cc932a540b1a0
disufi <d.isufi@gmail.com>
2014-12-02 09:55:07 +0100

    [new] dataload on pagenavigation
    first implementation in home

--- Quizio/Quizio/ViewModels/HomeViewModel.cs
+++ Quizio/Quizio/ViewModels/HomeViewModel.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.Practices.Prism.Mvvm;
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
 using Quizio.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Quizio.ViewModels
 {
@@ -12,9 +16,31 @@ namespace Quizio.ViewModels
     {
         public ModelAggregator Aggregator { get; set; }
 
+        private BackgroundWorker bw;
+
         public HomeViewModel(ModelAggregator aggregator)
         {
             this.Aggregator = aggregator;
+
+            bw = new BackgroundWorker();
+            bw.DoWork += bw_DoWork;
+        }
+
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                Aggregator.loadData();
+            }
+            catch (Exception ex)
+            {
+                ModernDialog.ShowMessage(ex.Message, "Fehler mit der Verbingung!", System.Windows.MessageBoxButton.OK);
+            }
+        }
+
+        public void ReloadHomeData()
+        {
+            bw.RunWorkerAsync();
         }
     }
 }
--- Quizio/Quizio/Views/Home.xaml.cs
+++ Quizio/Quizio/Views/Home.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FirstFloor.ModernUI.Windows;
+using FirstFloor.ModernUI.Windows.Controls;
+using Quizio.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +21,33 @@ namespace Quizio.Views
     /// <summary>
     /// Interaction logic for Home.xaml
     /// </summary>
-    public partial class Home : UserControl
+    public partial class Home : UserControl, IContent
     {
         public Home()
         {
             InitializeComponent();
+        }
+
+
+        public void OnFragmentNavigation(FirstFloor.ModernUI.Windows.Navigation.FragmentNavigationEventArgs e)
+        {
+        }
+
+        public void OnNavigatedFrom(FirstFloor.ModernUI.Windows.Navigation.NavigationEventArgs e)
+        {
+        }
+
+        public void OnNavigatedTo(FirstFloor.ModernUI.Windows.Navigation.NavigationEventArgs e)
+        {
+            HomeViewModel hvm = this.DataContext as HomeViewModel;
+            if (hvm != null)
+            {
+                hvm.ReloadHomeData();
+            }
+        }
+
+        public void OnNavigatingFrom(FirstFloor.ModernUI.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
         }
     }
 }
